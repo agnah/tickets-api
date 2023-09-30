6bcf4c20f2059a2553586e4006b94aa83a8084ad
agnah <aizquierdojoffre@gmail.com>
2023-09-30 12:20:20 -0300

    Agregando configuración para permitir comunicación desde fuera del dominio de la api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import Settings
 
@@ -8,6 +9,20 @@ from app.endpoints.area import router as area_router
 
 app = FastAPI()
 setings = Settings()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routers
 app.include_router(healthcheck_router, prefix="/api/healthcheck", tags=["Healthcheck"])
